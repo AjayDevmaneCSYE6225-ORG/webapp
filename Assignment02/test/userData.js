68cbf85caafa3e1acbb48b3be2ba690e351e578b
ajaysubashdevmane <ajaysubashdevmane@Hephaestus.local>
2024-02-15 01:56:50 -0500

    adding integration workflow

---
+++ Assignment02/test/userData.js
@@ -0,0 +1,18 @@
+const userData={
+    "username": "daredevil@gmail.com",
+    "password": "Daredevil@123",
+    "firstName": "Dare",
+    "lastName": "Devil"
+};
+
+const updateUserData={
+    "password": "Mareangel@123",
+    "firstName": "Mare",
+    "lastName": "Angel"
+}
+
+module.exports= {
+    userData,
+    updateUserData
+};
+
