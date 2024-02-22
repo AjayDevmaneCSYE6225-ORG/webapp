928c8cf65d62d59abdd7466af869b2b2015d4821
ajaysubashdevmane <ajaysubashdevmane@Hephaestus.local>
2024-02-21 20:13:31 -0500

    packer trial transfer

--- Assignment02/packer/unzip-service.sh
+++ Assignment02/packer/unzip-service.sh
@@ -3,7 +3,9 @@
 sudo mkdir /tmp/unzippedWebapp
 sudo mv /tmp/webapp.zip /opt/
 sudo unzip -o /opt/webapp.zip -d /opt/unzippedWebapp
+
 cd /opt/unzippedWebapp/Assignment02
+sudo rm -rf node_modules
 sudo npm install
 sudo mv /opt/unzippedWebapp/Assignment02/csye6225.service /etc/systemd/system
 
