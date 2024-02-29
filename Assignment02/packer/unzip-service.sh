d2b9f77689c74ddffd74c41390b1dca32cdd672b
ajaysubashdevmane <ajaysubashdevmane@Hephaestus.local>
2024-02-28 23:30:17 -0500

    removed mysql installation

--- Assignment02/packer/unzip-service.sh
+++ Assignment02/packer/unzip-service.sh
@@ -11,13 +11,13 @@ sudo unzip -o /opt/webapp.zip -d /opt/unzippedWebapp
 sudo chown -R csye6225:csye6225 /opt/unzippedWebapp/Assignment02
 sudo mv /tmp/.env /opt/unzippedWebapp/Assignment02
 
-sudo systemctl enable mysqld
-
 cd /opt/unzippedWebapp/Assignment02
 sudo rm -rf node_modules
 
 sudo npm install
+
 sudo mv /opt/unzippedWebapp/Assignment02/csye6225.service /etc/systemd/system
 
 sudo systemctl daemon-reload
 sudo systemctl enable csye6225.service
+sudo systemctl start csye6225.service
