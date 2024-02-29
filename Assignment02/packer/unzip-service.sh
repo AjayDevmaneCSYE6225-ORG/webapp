7d9c04d56cbccfff484cc6f3080f16fbe95a3c8f
ajaysubashdevmane <ajaysubashdevmane@Hephaestus.local>
2024-02-28 23:45:52 -0500

    updated packer

--- Assignment02/packer/unzip-service.sh
+++ Assignment02/packer/unzip-service.sh
@@ -9,7 +9,6 @@ sudo usermod -s /sbin/nologin csye6225
 sudo mv /tmp/webapp.zip /opt/
 sudo unzip -o /opt/webapp.zip -d /opt/unzippedWebapp
 sudo chown -R csye6225:csye6225 /opt/unzippedWebapp/Assignment02
-sudo mv /tmp/.env /opt/unzippedWebapp/Assignment02
 
 cd /opt/unzippedWebapp/Assignment02
 sudo rm -rf node_modules
