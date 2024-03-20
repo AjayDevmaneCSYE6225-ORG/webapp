86fb6752d6e751b293f829bf6233821e74c4faa6
ajaysubashdevmane <ajaysubashdevmane@Hephaestus.local>
2024-03-20 15:04:32 -0400

    config changes

--- Assignment02/packer/ops.sh
+++ Assignment02/packer/ops.sh
@@ -3,6 +3,12 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
+sudo chown -R csye6225:csye6225 /var/log
+
+sudo chmod -R 775 /etc/google-cloud-ops-agent/
+
 sudo mv /opt/unzippedWebapp/Assignment02/config.yaml /etc/google-cloud-ops-agent/config.yaml
+
+sudo chmod -R 775 /etc/google-cloud-ops-agent/config.yaml
 
 sudo systemctl restart google-cloud-ops-agent
